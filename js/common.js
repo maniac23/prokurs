2dfaab0f2ed9dda9ea75058d3230eab4357e418c
Mikhail Tukai <tukaym@gmail.com>
2016-03-10 20:20:21 +0300

    initial commit

---
+++ js/common.js
@@ -0,0 +1,101 @@
+$(document).ready(function () {
+
+    $.stellar({
+        responsive: true,
+        horizontalOffset: 60
+    });
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            }
+        },
+        navText: ""
+    });
+    $('.popup').magnificPopup({
+        type: 'image'
+    });
+    $('.popup_c').magnificPopup();
+
+
+
+    function wResize() {
+        $("header").css("min-height", $(window).height())
+    };
+    wResize();
+    $(window).resize(function () {
+        wResize();
+    });
+    $(".top_phone .wrapper .tab").click(function () {
+        $(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+
+
+    $(".bottom_phone .wrapper .tab").click(function () {
+        $(".bottom_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".bottom_phone .tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+
+
+    $(".tabs_header .wrapper .tab").click(function () {
+        $(".tabs_header .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+
+    $(".contacts_top .tab").click(function () {
+        $(".contacts_top .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".s_contacts .tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+
+    //Цели для Яндекс.Метрики и Google Analytics
+    $(".count_element").on("click", (function () {
+        ga("send", "event", "goal", "goal");
+        yaCounterXXXXXXXX.reachGoal("goal");
+        return true;
+    }));
+
+    //SVG Fallback
+    if (!Modernizr.svg) {
+        $("img[src*='svg']").attr("src", function () {
+            return $(this).attr("src").replace(".svg", ".png");
+        });
+    };
+
+    //Аякс отправка форм
+    //Документация: http://api.jquery.com/jquery.ajax/
+    $("form").submit(function (e) {
+        var ths = $(this);
+        e.preventDefault;
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: $(this).serialize()
+        }).done(function () {
+            alert("Спасибо за заявку!");
+            setTimeout(function () {
+                var magnificPopup = $.magnificPopup.instance;
+                magnificPopup.close();
+                ths.trigger("reset");
+            }, 1000);
+
+        });
+        return false;
+    });
+
+});
+$(window).load(function () {
+    $(".top_header h1").animated("fadeInDown", "fadeOut");
+    $(".top_header h2").animated("fadeInUp", "fadeOut");
+    $(".tabs_header .wrapper").animated("flipInY", "fadeOut");
+    $(".profi_item").animated("fadeInRight", "fadeOut");
+    $(".s_profi form").animated("zoomInRight", "fadeOut");
+    $(".s_back h3").animated("fadeInUp", "fadeOut");
+    $("footer").animated("fadeIn", "fadeOut");
+
+
+
+
+});
